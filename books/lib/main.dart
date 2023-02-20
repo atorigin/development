62e39cd55d380515d7b93c99f2eb4467157b4e43
atorigin <qaz60908@gmail.com>
2023-02-20 22:43:17 +0800

    移除 test 目錄 , 新增 dart 常用語法練習

--- books/lib/main.dart
+++ books/lib/main.dart
@@ -1,115 +1,67 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+void main(List<String> args) {
+  print("練習 for");
+  for(var i = 5; i >= 1 ; i--) {
+    print(i);  
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  print("練習 forEach");
+  var col = [1,2,3,4,5];
+  col.forEach((element) {print(element);});
+
+  print("練習 map , 對 collection 每一個 elem 操作");
+  List<int> col2 = [5,6,7,8];
+  var col2_new = col2.map((e) => e+1);
+  col2_new.forEach((element) {print(element);});
+
+  print("認識 dart 特殊符號");
+  // dart 特殊符號
+  // ? 代表該 var 有可能為 null
+  // ! 不管該 var 是否為 null , 請依照一般情況處理
+
+  int? maybeNull;
+  int notBeNull;
+  print(maybeNull);
+  // print(notBeNull); // 這行在 compiler 就會出錯
+
+  List<int>? maybeNull2;
+  // maybeNull2!.forEach((element) {print(element);}); // 沒有驚嘆號 , 在 compile 時就有錯
+
+  // 關於 ... 符號
+  // 陣列合併的情形
+  List list1 = ["1", "2"];
+  List list2 = ["3", "4"];
+  List list3 = ["5", "6"];
+  var totalList = list1 + list2 + list3;
+  print(totalList);
+  var totalList2 = [...list1,...list2,...list3]; // ... 符號用於 List 合併
+  print(totalList2);
+
+  print("練習 dart if-else");
+  int num;
+  if(10>5) {
+    num = 5;
+  } else {
+    num = 3;
   }
+  print(num);
+
+  print("單句賦值判斷式");
+  //(布林條件式) ? 若為是，回傳值 : 若為否, 回傳值
+  int x = 5; // 可以換成 11 看看結果有無差異
+  10>x ? print(10) : print(5);
+
+  print("練習 dart 的 function");
+  // 無回傳 function
+  void demoNotReturnFunc(int temp) {
+    print(temp);
+  }
+  demoNotReturnFunc(87);
+
+  // 有回傳 function
+  int demoReturnNumberFunc(int temp) {
+    return temp + 100;
+  }
+  print(demoReturnNumberFunc(87));
+
+  // 另外還有匿名函式與類別的使用，因語法基本與 Java 類似，故這邊就沒特別練習
 }
