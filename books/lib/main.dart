c4c311ae40e1a0ec444c5e2691acacb3ea77a3d5
atorigin <qaz60908@gmail.com>
2023-02-21 13:29:13 +0800

    練習 chapter02-1 homework 五個範例

--- books/lib/main.dart
+++ books/lib/main.dart
@@ -1,13 +1,27 @@
 void main(List<String> args) {
-  String name = "小菜";
-  String companyName = "cxcxc";
-  int age = 24;
+  // Q1
+  String jobInfo = "全端工程師";
+  // Q2
+  String description = "會寫個用戶看的，也會接觸資料庫的";
+  // Q3
+  int salary = 35000;
+  // Q4
+  print("有一個職缺是：" + jobInfo + "負責處理" + description + "。目前該職缺的薪水為" + salary.toString());
+  print(description + jobInfo + "。他的薪水是：" + salary.toString());
 
-  // 各自打印
-  print(name);
-  print(companyName);
-  print(age.toString());
+  // Q5
+  Item aBook = Item();
+  aBook.description();
+}
 
-  // 組合打印
-  print("在" + companyName + "有一個員工 , 名字叫做" + name + "他今年" + age.toString() + "歲");
+// Q5 物件
+class Item {
+  String name = "大話AWS雲端架構";
+  int weight = 140;
+
+  Item();
+
+  void description() {
+    print("這個物品名稱是：" + this.name + "。重量為：" + this.weight.toString());
+  }
 }
