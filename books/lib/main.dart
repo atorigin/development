6554262d4f9bdfb290f9b9b290f3f5e7d3b6e4b7
atorigin <qaz60908@gmail.com>
2023-02-25 22:56:40 +0800

    練習chapter02-3 homework

--- books/lib/main.dart
+++ books/lib/main.dart
@@ -1,29 +1,58 @@
+void nameIsEqualAtorigin(String name) {
+  if(name == "atorigin") {
+    print("coding is live\n");
+  } else {
+    print("coding is live also is part of work\n");
+  }
+}
+void isMiddleClassFamily(int salary) {
+  double halfSalary = salary / 2;
+  double remainingSalary = halfSalary - 10000 - 5000;
+  if(remainingSalary > 30000) {
+    print("ya! middle-class family");
+  } else {
+    print("working-class family , I still go on to coding ~\n");
+  }
+}
+
+void isCompanyWorthToInvesting(int revenue) {
+  int staff = 5;
+  // 定義公司營業稅
+  double businessTax = revenue * 0.05;
+  print("businessTax : " + businessTax.toString());
+  // 雜項支出 = 公司基礎雜項支出 + 員工人數*每人每年雜支 + 員工人數*每人每年勞健保費用 + 員工人數*每人每年薪水
+  double miscellaneousExpenses = 2000000 + (5 * 12000) + (5 * 10000) + (5 * 40000 * 12);
+  print("miscellaneousExpenses : " + miscellaneousExpenses.toString());
+  // 定義公司營所稅
+  double incomeTax = (revenue - businessTax - miscellaneousExpenses) * 0.2;
+  print("incomeTax : " + incomeTax.toString());
+  // 計算公司總營業所得(盈餘)
+  double surplus = revenue - businessTax - miscellaneousExpenses - incomeTax;
+  print("surplus : " + surplus.toString());
+  // 計算股票淨值
+  double stockValue = (revenue - businessTax - miscellaneousExpenses - incomeTax) / 1000000;
+  print("stockValue : " + stockValue.toString());
+
+  // 判斷股票淨值大於 5 ; 介於 2~5 ; 小於 2
+  if(stockValue > 5) {
+    print("好投資標的");
+  } else if(stockValue > 2) {
+    print("可投資標的");
+  } else {
+    print("建議看其他家");
+  }
+}
+
 void main(List<String> args) {
-  print("情境一：若現場人數超過50，則老王女友嫁給老王，否則就一起去吃東西\n");
+  print("情境一：判斷字串是否為 atorigin , 打印出不同訊息");
 
-  int peopleAmount = 51;
-  if(peopleAmount > 50) {
-    print("老王女友答應嫁給老王");
-  } else {
-    print("老王女友和老王一起去吃東西");
-  }
+  nameIsEqualAtorigin("atorigin");
 
-  print("\n情境二：雖然現場人數超過50人，但老王女友說出父母的顧慮，要求老王存款若大於三萬則立刻嫁，若小於三萬且大於一萬，則勉強可以，若小於一萬則鼓勵老王在努力工作存錢\n");
+  print("情境二： 判斷是否為小康家庭");
 
-  int budget = 30000;
-  if(budget >= 30000) {
-    print("老王女友決定馬上嫁給老王");
-  } else if(budget > 10000) {
-    print("老王女友思考了一下，認為可以勉強嫁給老王");
-  } else {
-    print("老王女友向老王說，我們還年輕，可以先努力存點錢再來打算");
-  }
+  isMiddleClassFamily(60000);
 
-  print("\n情境三：小菜女友小美說「如果嫁的對象是小菜，我才嫁。否則我終身都不結婚」\n");
-  String husbandName = "小菜";
-  if(husbandName != "小菜"){
-    print("我終身不嫁");
-  } else {
-    print("結婚吧");
-  }
+  print("情境三： 計算股票投資標的 , 公司預定營收為500萬");
+
+  isCompanyWorthToInvesting(5000000);
 }
