75f9fe398940ff42e9c2f2d30b58a02704fb7cc1
atorigin <qaz60908@gmail.com>
2023-02-25 20:07:14 +0800

    更新 README 說明 chapter02-2 homework 和練習 chapter02-2 範例 code

--- books/README.md
+++ books/README.md
@@ -7,3 +7,8 @@ Q2 : 有一個文字表達的變數，其變數名為 description，其內容叫
 Q3 : 有一個數字表達的變數，其變數名為 salary，其內容為 35000   
 Q4 : 將這些內容組合成一個大型字串，並使用 print 印在畫面上   
 Q5 : 有一個東西，他有兩個特徵，名字是大話 AWS 雲端架構、重量是 140，請用 Dart 程式語言的方式 對這兩個變數進行宣告   
+
+## chapter02-2 homework
+Q1 : 定義一個會回傳字串的方法，函數名為 iAmStringFunction，並帶有字串型別的函數參數 iAmString, 之後回傳字串為 HelloWorld+該參數的內容   
+Q2 : 定義一個會回傳數字的方法，函數名為 iAmIntFunction，並帶有字串型別的函數參數 iAmInt, 之後回傳數字為 該參數*20
+Q3 : 定義一個不回傳內容的方法，函數名為 iWillNotReturnValue，在裡面設置 print("通常會用於 與資料庫、硬碟等外部系統接觸的情境")
--- books/lib/main.dart
+++ books/lib/main.dart
@@ -1,27 +1,37 @@
-void main(List<String> args) {
-  // Q1
-  String jobInfo = "全端工程師";
-  // Q2
-  String description = "會寫個用戶看的，也會接觸資料庫的";
-  // Q3
-  int salary = 35000;
-  // Q4
-  print("有一個職缺是：" + jobInfo + "負責處理" + description + "。目前該職缺的薪水為" + salary.toString());
-  print(description + jobInfo + "。他的薪水是：" + salary.toString());
-
-  // Q5
-  Item aBook = Item();
-  aBook.description();
+int returnIntWithoutParameter() {
+  int result = 20+1;
+  return result;
 }
 
-// Q5 物件
-class Item {
-  String name = "大話AWS雲端架構";
-  int weight = 140;
+double returnDoubleWithParameter(double inputDouble) {
+  double result = inputDouble * 100 / 20;
+  return result;
+}
 
-  Item();
+String returnStringWithParameter(String inputStr) {
+  String result = inputStr + "是一個字串";
+  return result;
+}
 
-  void description() {
-    print("這個物品名稱是：" + this.name + "。重量為：" + this.weight.toString());
-  }
+void withoutReturnJustExecute() {
+  print("沒有回傳資料的函數，用 void 宣告此方法的回傳值型別");
+}
+
+void main(List<String> args) {
+  print("====沒有參數的函數，取值====");
+  int getIntFromFunction = returnIntWithoutParameter();
+  print(getIntFromFunction);
+
+  print("====在函數動態輸入數字，取值====");
+  double getDoubleFromFunctionWithParam1 = returnDoubleWithParameter(2);
+  double getDoubleFromFunctionWithParam2 = returnDoubleWithParameter(5);
+  print(getDoubleFromFunctionWithParam1);
+  print(getDoubleFromFunctionWithParam2);
+
+  print("====在函數動態輸入文字，取值====");
+  String getStringFromFunction = returnStringWithParameter("atorigin");
+  print(getStringFromFunction);
+
+  print("====不需接收回傳值，調度函數====");
+  withoutReturnJustExecute();
 }
