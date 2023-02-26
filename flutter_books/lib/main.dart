ba15796ece07c1cd10ccfd690f1e557f3dd37447
atorigin <qaz60908@gmail.com>
2023-02-26 21:19:46 +0800

    練習 chapter02-4 homework

--- flutter_books/lib/main.dart
+++ flutter_books/lib/main.dart
@@ -1,17 +1,34 @@
+import 'dart:io';
+
 void main(List<String> args) {
-  List<String> nameList1 = ["王小明", "李小菜", "atorigin"];
+  // Q1
+  List<int> numList = [3,5,6,1,2];
+  int answer = 0;
 
-  print("List 元素取用方式");
-  print(nameList1[0]);
-  print(nameList1[1]);
-
-  print("\nfor loop 數字序列號版本");
-  for(int index=0; index < nameList1.length ; index++) {
-    print(nameList1[index]);
+  // Q2
+  for(int i=0; i < numList.length ; i++) { 
+    answer += numList[i];
   }
-  
-  print("\nfor loop 元素遍歷版");
-  for(String e in nameList1) {
-    print(e + "送早餐給小美");
+  print("Q2 Answer = " + answer.toString());
+
+  // Q3
+  answer = 0; // reset
+  for (int e in numList) {
+    answer += e;
+  }
+
+  print("Q3 Answer = " + answer.toString());
+
+  //Q4
+  for(int i = 1; i < 5 ; i++){
+    print("*" * i);
+  }
+
+  //Q5
+  for(int x = 1 ; x < 10 ; x++){
+    for(int y = 1 ; y < 10 ; y++) {
+      stdout.write(x.toString() + "*" + y.toString() + "=" + (x * y).toString() + "\t");
+    }
+    print("");
   }
 }
