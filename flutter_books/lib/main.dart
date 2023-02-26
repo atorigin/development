bf5e9d701f583d98286e477b04d26dc3442c8a98
atorigin <qaz60908@gmail.com>
2023-02-26 21:59:25 +0800

    練習 chapter02-5 範例

--- flutter_books/lib/main.dart
+++ flutter_books/lib/main.dart
@@ -1,34 +1,29 @@
-import 'dart:io';
-
 void main(List<String> args) {
-  // Q1
-  List<int> numList = [3,5,6,1,2];
-  int answer = 0;
+  Map<String,String> positionMap = {
+    "generalManager": "bing-hong",
+    "humanResource": "xiao-tsai",
+    "accounting": "xiao-mei"
+  };
 
-  // Q2
-  for(int i=0; i < numList.length ; i++) { 
-    answer += numList[i];
-  }
-  print("Q2 Answer = " + answer.toString());
+  // 利用 key 印出值為 bing-hong 的結果
+  print(positionMap["generalManager"]);
 
-  // Q3
-  answer = 0; // reset
-  for (int e in numList) {
-    answer += e;
-  }
+  // 為 Map 新增一組 key-value
+  positionMap["part-time"] = "xiao-black";
+  print(positionMap["part-time"]);
 
-  print("Q3 Answer = " + answer.toString());
-
-  //Q4
-  for(int i = 1; i < 5 ; i++){
-    print("*" * i);
+  // 利用迴圈拿到 key 逐步打印出其對應的 value
+  print("迴圈取值 use-case");
+  for(String key in positionMap.keys) {
+    print(positionMap[key]);
   }
 
-  //Q5
-  for(int x = 1 ; x < 10 ; x++){
-    for(int y = 1 ; y < 10 ; y++) {
-      stdout.write(x.toString() + "*" + y.toString() + "=" + (x * y).toString() + "\t");
-    }
-    print("");
+  // 有時會直接將取到的其他 Map 全部加入一個 Map 裡面
+  print("將新的 Map 加入原有 Map 的 use-case");
+  Map<String,String> newPositionMap = {"worker": "somebody"};
+  positionMap.addAll(newPositionMap);
+
+  for(String key in positionMap.keys){
+    print(positionMap[key]);
   }
 }
