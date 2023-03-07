3f7f8fc8606fb730b57d01e25e65218d3858ff33
atorigin <qaz60908@gmail.com>
2023-03-07 22:40:42 +0800

    練習 chapter02-12 homework

--- flutter_books/lib/main.dart
+++ flutter_books/lib/main.dart
@@ -1,40 +1,25 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-// 寫一個 getRemoteSystemData 的 function
-Future<List<dynamic>> getRemoteSystemData(String remoteUrl) async {
-  Uri url = Uri.parse(remoteUrl);
-  http.Response multipleResponse = await http.get(url);
-  List<dynamic> jsonArrayFromRemote = jsonDecode(multipleResponse.body);
-  return jsonArrayFromRemote;
-}
 
-// 寫一個 getRemoteSystemDataWithoutAsync
-Future<List<dynamic>> getRemoteSystemDataWithoutAsync(String remoteUrl) {
+Future<Map<String,dynamic>> getRemoteSystemDataTheObject(String remoteUrl, int count) async {
   Uri url = Uri.parse(remoteUrl);
-  Future<http.Response> multipleResponseWithFuture = http.get(url);
-  Future<List<dynamic>> jsonArrayFromRemoteWithFuture = multipleResponseWithFuture.then((responses) => jsonDecode(responses.body));
-  return jsonArrayFromRemoteWithFuture;
+  List<dynamic> jsonArrayFromRemote = jsonDecode((await http.get(url)).body);
+  Map<String, dynamic> jsonObjectFromRemote = jsonArrayFromRemote[count];
+  return jsonObjectFromRemote;
 }
-
-// main 方法，使用 getRemoteSystemData 的 function 和 getRemoteSystemDataWithoutAsync 的 function 各操作一次
 void main(List<String> args) async {
-  // 利用第一個 Function 取得資料並抓取對應欄位
-  // 要調用 await 的話 , 該 function 必定要是一個 async function , 因此 main 需宣告為 async
-  List<dynamic> f1 = await getRemoteSystemData("https://jsonplaceholder.typicode.com/users");
-  print(JsonEncoder.withIndent("    ").convert(f1[0]));
-  print(f1[0]["username"]);
-  print(f1[0]["email"]);
-  // 如何避免 null
-  print(f1[0]["family"]);
+  // Q1 https://jsonplaceholder.typicode.com/posts
+  Map<String, dynamic> question1Object = await getRemoteSystemDataTheObject("https://jsonplaceholder.typicode.com/posts", 4);
+  print(question1Object["id"]);
+  // Q2 https://jsonplaceholder.typicode.com/comments
+  Map<String, dynamic> question2Object = await getRemoteSystemDataTheObject("https://jsonplaceholder.typicode.com/comments", 4);
+  print(question2Object["id"]);
+  // Q3 https://jsonplaceholder.typicode.com/albums
+  Map<String, dynamic> question3Object = await getRemoteSystemDataTheObject("https://jsonplaceholder.typicode.com/albums", 4);
+  print(question3Object["id"]);
+  // Q4 https://jsonplaceholder.typicode.com/todos
+  Map<String, dynamic> question4Object = await getRemoteSystemDataTheObject("https://jsonplaceholder.typicode.com/todos", 4);
+  print(question4Object["id"]);
 
-  print("==========分隔線==========");
-
-  // 同上，但用第二個 Function 展示
-  List<dynamic> f2 = await getRemoteSystemDataWithoutAsync("https://jsonplaceholder.typicode.com/users");
-  print(JsonEncoder.withIndent("    ").convert(f2[0]));
-  print(f2[0]["username"]);
-  print(f2[0]["email"]);
-  // 同上 , 如何避免 null
-  print(f2[0]["family"]);
 }
