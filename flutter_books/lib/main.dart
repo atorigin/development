52865dee95004ebdc54efba4aae7956b3222bf7d
atorigin <qaz60908@gmail.com>
2023-03-11 13:44:50 +0800

    練習 chapter02-15 範例

--- flutter_books/lib/main.dart
+++ flutter_books/lib/main.dart
@@ -1,89 +1,52 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class Cat {
-  String variety;
-  int weight;
+// 建立一個 User class 其特徵為遠端系統類似之特徵 https://jsonplaceholder.typicode.com/users
+class User {
+  int id;
+  String name;
+  String username;
+  String email;
 
-  Cat(this.variety, this.weight);
+  User(this.id, this.name, this.username, this.email);
 
-  factory Cat.fromJson(dynamic catJson) {
-    String variety = catJson["variety"];
-    int weight = catJson["weight"];
-    return Cat(variety, weight);
+  factory User.fromJson(dynamic userJson) {
+    int id = userJson["id"];
+    String name = userJson["name"];
+    String username = userJson["username"];
+    String email = userJson["email"];
+    return User(id , name , username, email);
   }
 
-  void echo() {
-    print("喵");
+  void printUserInfo() {
+    print("有一個用戶 $name , 他的使用者帳號為 $username , 用戶 id 是 $id, 信箱為 $email");
   }
 
+  // 這邊其實可以宣告為 Map<String, dynamic> 更符合 toJson 格式，不過在 post data 到遠端時也是要多做一層 jsonEncode
   String toJson() {
-    dynamic instance = {"variety": variety, "weight": weight};
+    dynamic instance = {"id": id, "name": name, "username": username, "email": email};
     return jsonEncode(instance);
   }
 }
 
-class Dog {
-  String variety;
-  int weight;
+// 建立一個 function 取得遠端系統的 User 物件陣列
+Future<List<User>> getRemoteSystemUserData() async {
+  Uri multipleItemUrl = Uri.parse("https://jsonplaceholder.typicode.com/users");
+  http.Response remoteSystemResponses = await http.get(multipleItemUrl);
+  List<dynamic> jsonArrayFromRemote = jsonDecode(remoteSystemResponses.body);
+  // important: 轉成 User 物件的 Array
+  List<User> userArray = jsonArrayFromRemote.map((dynamicUserJson) => User.fromJson(dynamicUserJson)).toList();
+  return userArray;
+}
+
+// main 方法，將 user 物件轉換並 post 到遠端系統，將 post 回傳的 response 結果打印
+void main(List<String> args) async {
+  List<User> userArrayFromRemote = await getRemoteSystemUserData();
+  print("=====打印 User 陣列第一個元素的信息=====");
+  print(userArrayFromRemote[0].toJson());
   
-  Dog(this.variety, this.weight);
-
-  factory Dog.fromJson(dynamic dogJson) {
-    String variety = dogJson["variety"];
-    int weight = dogJson["weight"];
-    return Dog(variety, weight);
-  }
-
-  void echo() {
-    print("汪");
-  }
-
-  String toJson() {
-    dynamic instance = {"variety": variety, "weight": weight};
-    return jsonEncode(instance);
-  }
-}
-
-class Human {
-  String variety;
-  int weight;
-
-  Human(this.variety, this.weight);
-
-  factory Human.fromJson(dynamic humanJson) {
-    String variety = humanJson["variety"];
-    int weight = humanJson["weight"];
-    return Human(variety, weight);
-  }
-
-  void echo() {
-    print("Hello");
-  }
-
-  String toJson() {
-    dynamic instance = {"variety": variety, "weight": weight};
-    return jsonEncode(instance);
-  }
-}
-
-void main(List<String> args) {
-  // 生成 Cat 物件
-  Cat myCat = Cat("加菲貓", 30);
-  myCat.echo();
-  print(myCat.toJson());
-
-  // 生成 Dog 物件
-  Dog myDog = Dog("柴犬", 28);
-  myDog.echo();
-  print(myDog.toJson());
-
-  // 生成 Human 物件
-  Human me = Human("男性", 70);
-  me.echo();
-  print(me.toJson());
-
-  String anotherCat = """{"variety": "黑貓", "weight": 15}""";
-  Cat cat2 = Cat.fromJson(jsonDecode(anotherCat));
-  cat2.echo();
-  print(cat2.toJson().runtimeType);
+  Uri url = Uri.parse("https://jsonplaceholder.typicode.com/users");
+  String postData = userArrayFromRemote[0].toJson();
+  http.Response postResponse = await http.post(url, body: postData);
+  print(postResponse.body);
 }
