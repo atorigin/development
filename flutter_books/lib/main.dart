f40ba0db3419daa63a1291d280f8c065c91744b0
atorigin <qaz60908@gmail.com>
2023-03-11 14:39:45 +0800

    練習 chapter02-15 homework

--- flutter_books/lib/main.dart
+++ flutter_books/lib/main.dart
@@ -1,52 +1,114 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-// 建立一個 User class 其特徵為遠端系統類似之特徵 https://jsonplaceholder.typicode.com/users
-class User {
+class Post {
   int id;
-  String name;
-  String username;
-  String email;
+  String title;
+  String body;
 
-  User(this.id, this.name, this.username, this.email);
+  Post(this.id, this.title, this.body);
 
-  factory User.fromJson(dynamic userJson) {
-    int id = userJson["id"];
-    String name = userJson["name"];
-    String username = userJson["username"];
-    String email = userJson["email"];
-    return User(id , name , username, email);
+  factory Post.fromJson(dynamic postJson) {
+    int id = postJson["id"];
+    String title = postJson["title"];
+    String body = postJson["body"];
+    return Post(id, title, body);
   }
 
-  void printUserInfo() {
-    print("有一個用戶 $name , 他的使用者帳號為 $username , 用戶 id 是 $id, 信箱為 $email");
-  }
-
-  // 這邊其實可以宣告為 Map<String, dynamic> 更符合 toJson 格式，不過在 post data 到遠端時也是要多做一層 jsonEncode
   String toJson() {
-    dynamic instance = {"id": id, "name": name, "username": username, "email": email};
+    dynamic instance = {"id": id, "title": title, "body": body};
     return jsonEncode(instance);
   }
 }
 
-// 建立一個 function 取得遠端系統的 User 物件陣列
-Future<List<User>> getRemoteSystemUserData() async {
-  Uri multipleItemUrl = Uri.parse("https://jsonplaceholder.typicode.com/users");
-  http.Response remoteSystemResponses = await http.get(multipleItemUrl);
-  List<dynamic> jsonArrayFromRemote = jsonDecode(remoteSystemResponses.body);
-  // important: 轉成 User 物件的 Array
-  List<User> userArray = jsonArrayFromRemote.map((dynamicUserJson) => User.fromJson(dynamicUserJson)).toList();
-  return userArray;
+class Comment {
+  int id;
+  String name;
+  String email;
+
+  Comment(this.id, this.name, this.email);
+
+  factory Comment.fromJson(dynamic commentJson) {
+    int id = commentJson["id"];
+    String name = commentJson["name"];
+    String email = commentJson["email"];
+    return Comment(id, name, email);
+  }
+
+  String toJson() {
+    dynamic instance = {"id": id, "name": name, "email": email};
+    return jsonEncode(instance);
+  }
 }
 
-// main 方法，將 user 物件轉換並 post 到遠端系統，將 post 回傳的 response 結果打印
+class Album {
+  int userId;
+  int id;
+  String title;
+
+  Album(this.userId, this.id, this.title);
+
+  factory Album.fromJson(dynamic albumJson) {
+    int userId = albumJson["userId"];
+    int id = albumJson["id"];
+    String title = albumJson["title"];
+    return Album(userId, id, title);
+  }
+
+  String toJson() {
+    dynamic instance = {"userId": userId, "id": id, "title": title};
+    return jsonEncode(instance);
+  }
+}
+
+class Todo {
+  int id;
+  String title;
+  bool completed;
+
+  Todo(this.id, this.title, this.completed);
+
+  factory Todo.fromJson(dynamic todoJson) {
+    int id = todoJson["id"];
+    String title = todoJson["title"];
+    bool completed = todoJson["completed"];
+    return Todo(id, title, completed);
+  }
+
+  String toJson() {
+    dynamic instance = {"id": id, "title": title, "completed": completed};
+    return jsonEncode(instance);
+  }
+}
+
 void main(List<String> args) async {
-  List<User> userArrayFromRemote = await getRemoteSystemUserData();
-  print("=====打印 User 陣列第一個元素的信息=====");
-  print(userArrayFromRemote[0].toJson());
+  // 宣告 Q1 ~ Q4 Url 物件
+  Uri url01 = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+  Uri url02 = Uri.parse("https://jsonplaceholder.typicode.com/comments");
+  Uri url03 = Uri.parse("https://jsonplaceholder.typicode.com/albums");
+  Uri url04 = Uri.parse("https://jsonplaceholder.typicode.com/todos");
   
-  Uri url = Uri.parse("https://jsonplaceholder.typicode.com/users");
-  String postData = userArrayFromRemote[0].toJson();
-  http.Response postResponse = await http.post(url, body: postData);
-  print(postResponse.body);
+  // 取得四個遠端系統的 Item List 轉成對應的物件 List
+  List<dynamic> postArrayResponses = jsonDecode((await http.get(url01)).body);
+  List<Post> postArray = postArrayResponses.map((e) => Post.fromJson(e)).toList();
+  
+  List<dynamic> commentArrayResponses = jsonDecode((await http.get(url02)).body);
+  List<Comment> commentArray = commentArrayResponses.map((e) => Comment.fromJson(e)).toList();
+
+  List<dynamic> albumArrayResponses = jsonDecode((await http.get(url03)).body);
+  List<Album> albumArray = albumArrayResponses.map((e) => Album.fromJson(e)).toList();
+
+  List<dynamic> todoArrayResposnes = jsonDecode((await http.get(url04)).body);
+  List<Todo> todoArray = todoArrayResposnes.map((e) => Todo.fromJson(e)).toList();
+
+  // 都取第五個 item 作為 post data 送到 remote system
+  http.Response postResponse1 = await http.post(url01, body: postArray[4].toJson());
+  print("第一個 Response = ${postResponse1.body}");
+  http.Response postResponse2 = await http.post(url02, body: commentArray[4].toJson());
+  print("第二個 Response = ${postResponse2.body}");
+  http.Response postResponse3 = await http.post(url03, body: albumArray[4].toJson());
+  print("第三個 Response = ${postResponse3.body}");
+  http.Response postResponse4 = await http.post(url04, body: todoArray[4].toJson());
+  print("第四個 Response = ${postResponse4.body}");
+
 }
